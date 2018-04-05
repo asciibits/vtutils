d34a46745bf0873054119e8c3b140c34349eb45f
Larry Sanderson <34527442+asciibits@users.noreply.github.com>
2018-04-04 20:29:11 -0700

    Start work on the curses implementation of screen. Fix a few headers too.

---
+++ src/curses_screen.cc
@@ -0,0 +1,149 @@
+#include "curses_screen.h"
+
+#include <iostream>
+
+namespace vtutils {
+namespace screen {
+
+void CursesScreen::reset() {
+  std::cout << "CursesScreen#reset: " << std::endl;
+  _flags = 0;
+}
+void CursesScreen::hard_reset() {
+  std::cout << "CursesScreen#hard_reset: " << std::endl;
+}
+
+void CursesScreen::set_flags(unsigned int flags) {
+  unsigned int old = _flags;
+  _flags |= flags;
+  if ((old & SCREEN_ALTERNATE) != (_flags & SCREEN_ALTERNATE)) {
+    // ncurses always runs in alternate mode... emulate using an
+    // offline buffer
+    // TODO
+  }
+  if ((old & SCREEN_HIDE_CURSOR) != (_flags & SCREEN_HIDE_CURSOR)) {
+    // un-invert current cursor pos
+    // TODO
+  }
+  if ((old & SCREEN_INVERSE) != (_flags & SCREEN_INVERSE)) {
+    // Invert the entire screen
+    // TODO
+  }
+  
+  std::cout << "CursesScreen#set_flags: " << flags << std::endl;
+}
+void CursesScreen::reset_flags(unsigned int flags) {
+  std::cout << "CursesScreen#reset_flags: " << flags << std::endl;
+}
+
+void CursesScreen::write(char32_t sym, Attr *attr) {
+  std::cout << "CursesScreen#write: " << sym << ", " << *attr << std::endl;
+}
+void CursesScreen::newline() {
+  std::cout << "CursesScreen#newline: " << std::endl;
+}
+void CursesScreen::insert_lines(unsigned int num) {
+  std::cout << "CursesScreen#insert_lines: " << num << std::endl;
+}
+void CursesScreen::delete_lines(unsigned int num) {
+  std::cout << "CursesScreen#delete_lines: " << num << std::endl;
+}
+void CursesScreen::insert_chars(unsigned int num) {
+  std::cout << "CursesScreen#insert_chars: " << num << std::endl;
+}
+void CursesScreen::delete_chars(unsigned int num) {
+  std::cout << "CursesScreen#delete_chars: " << num << std::endl;
+}
+// system bell
+void CursesScreen::alert() {
+  std::cout << "CursesScreen#alert: " << std::endl;
+}
+
+Attr CursesScreen::default_attr() {
+  std::cout << "CursesScreen#default_attr: " << std::endl;
+  return Attr{};
+}
+
+void CursesScreen::set_def_attr(Attr attr) {
+  std::cout << "CursesScreen#set_def_attr: " << attr << std::endl;
+}
+
+void CursesScreen::move_left(unsigned int num) {
+  std::cout << "CursesScreen#move_left: " << num << std::endl;
+}
+void CursesScreen::move_right(unsigned int num) {
+  std::cout << "CursesScreen#move_right: " << num << std::endl;
+}
+void CursesScreen::move_to(unsigned int x, unsigned int y) {
+  std::cout << "CursesScreen#move_to: " << x << ", " << y << std::endl;
+}
+void CursesScreen::move_up(unsigned int num, bool scroll) {
+  std::cout << "CursesScreen#move_up: " << num << ", " << scroll << std::endl;
+}
+void CursesScreen::move_down(unsigned int num, bool scroll) {
+  std::cout << "CursesScreen#move_down: " << num << ", " << scroll << std::endl;
+}
+void CursesScreen::move_line_home() {
+  std::cout << "CursesScreen#move_line_home: " << std::endl;
+}
+
+void CursesScreen::scroll_up(unsigned int num) {
+  std::cout << "CursesScreen#scroll_up: " << num << std::endl;
+}
+void CursesScreen::scroll_down(unsigned int num) {
+  std::cout << "CursesScreen#scroll_down: " << num << std::endl;
+}
+
+void CursesScreen::set_tabstop() {
+  std::cout << "CursesScreen#set_tabstop: " << std::endl;
+}
+void CursesScreen::reset_tabstop() {
+  std::cout << "CursesScreen#reset_tabstop: " << std::endl;
+}
+void CursesScreen::reset_all_tabstops() {
+  std::cout << "CursesScreen#reset_all_tabstops: " << std::endl;
+}
+void CursesScreen::tab_right(unsigned int num) {
+  std::cout << "CursesScreen#tab_right: " << num << std::endl;
+}
+void CursesScreen::tab_left(unsigned int num) {
+  std::cout << "CursesScreen#tab_left: " << num << std::endl;
+}
+
+unsigned int CursesScreen::get_cursor_x() {
+  std::cout << "CursesScreen#get_cursor_x: " << std::endl;
+  return 1;
+}
+unsigned int CursesScreen::get_cursor_y() {
+  std::cout << "CursesScreen#get_cursor_y: " << std::endl;
+  return 2;
+}
+
+void CursesScreen::erase_screen(bool protect) {
+  std::cout << "CursesScreen#erase_screen: " << protect << std::endl;
+}
+void CursesScreen::erase_cursor_to_screen(bool protect) {
+  std::cout << "CursesScreen#erase_cursor_to_screen: " << protect << std::endl;
+}
+void CursesScreen::erase_screen_to_cursor(bool protect) {
+  std::cout << "CursesScreen#erase_screen_to_cursor: " << protect << std::endl;
+}
+void CursesScreen::erase_cursor_to_end(bool protect) {
+  std::cout << "CursesScreen#erase_cursor_to_end: " << protect << std::endl;
+}
+void CursesScreen::erase_home_to_cursor(bool protect) {
+  std::cout << "CursesScreen#erase_home_to_cursor: " << protect << std::endl;
+}
+void CursesScreen::erase_current_line(bool protect) {
+  std::cout << "CursesScreen#erase_current_line: " << protect << std::endl;
+}
+void CursesScreen::erase_chars(unsigned int num) {
+  std::cout << "CursesScreen#erase_chars: " << num << std::endl;
+}
+
+void CursesScreen::set_margins(unsigned int top, unsigned int bottom) {
+  std::cout << "CursesScreen#set_margins: " << top << ", " << bottom << std::endl;
+}
+
+} // namespace screen
+} // namespace vtutils
---
+++ src/curses_screen.h
@@ -0,0 +1,75 @@
+#ifndef CURSES_SCREEN_H
+#define CURSES_SCREEN_H
+
+#include <uchar.h>
+
+#include "config.h"
+#include "curses.h"
+#include "screen.h"
+
+// Use wide characters
+#define NCURSES_WIDECHAR 1
+
+namespace vtutils {
+namespace screen {
+
+class CursesScreen : public Screen {
+public:
+  CursesScreen(WINDOW *win): _win(win) {}
+  
+  void reset() override;
+  void hard_reset() override;
+
+  void set_flags(unsigned int flags) override;
+  void reset_flags(unsigned int flags) override;
+
+  void write(char32_t sym, Attr *attr) override;
+  void newline() override;
+  void insert_lines(unsigned int num) override;
+  void delete_lines(unsigned int num) override;
+  void insert_chars(unsigned int num) override;
+  void delete_chars(unsigned int num) override;
+  void alert() override;
+
+  Attr default_attr() override;
+  void set_def_attr(Attr attr) override;
+
+  void move_left(unsigned int num) override;
+  void move_right(unsigned int num) override;
+  void move_up(unsigned int num, bool scroll) override;
+  void move_down(unsigned int num, bool scroll) override;
+  void move_to(unsigned int x, unsigned int y) override;
+  void move_line_home() override;
+
+  void scroll_up(unsigned int num) override;
+  void scroll_down(unsigned int num) override;
+
+  void set_tabstop() override;
+  void reset_tabstop() override;
+  void reset_all_tabstops() override;
+  void tab_right(unsigned int num) override;
+  void tab_left(unsigned int num) override;
+
+  unsigned int get_cursor_x() override;
+  unsigned int get_cursor_y() override;
+
+  void erase_screen(bool protect) override;
+  void erase_cursor_to_screen(bool protect) override;
+  void erase_screen_to_cursor(bool protect) override;
+  void erase_cursor_to_end(bool protect) override;
+  void erase_home_to_cursor(bool protect) override;
+  void erase_current_line(bool protect) override;
+  void erase_chars(unsigned int num) override;
+  
+  void set_margins(unsigned int top, unsigned int bottom) override;
+  
+private:
+  WINDOW *const _win;
+  unsigned int _flags;
+};
+
+} // namespace screen
+} // namespace vtutils
+
+#endif /* CURSES_SCREEN_H */
+
